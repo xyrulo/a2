4732cffedeb16675b88a189db0eb3eb8b86b1193
xy <dialgadude20@gmail.com>
2023-11-11 14:46:25 -0800

    first publish

--- axe.cpp
+++ axe.cpp
@@ -1,5 +1,17 @@
+#include <stdio.h>
+#include <map>
+#include <bits/stdc++.h>
+
 using namespace std;
 
+string progName;
+vector<pair<int, string>> objcode;
+map<string, string> REG;
+map<string, pair<int, string>> OPTAB;
+map<string, int> SYMTAB;
+int LOCCTR = 0, pc = 0, base = 0, index = 0;
+
 int main(int argc, char *argv[]) {
+    return 0;
     
 }
