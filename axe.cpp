e30212f54c12f32fed99a43427f0e49210961079
xy <dialgadude20@gmail.com>
2023-11-11 15:29:31 -0800

    added fusheng's work

--- axe.cpp
+++ axe.cpp
@@ -2,15 +2,160 @@
 #include <map>
 #include <fstream>
 #include <bits/stdc++.h>
+#include <iostream>
+#include <string>
+#include <vector>
+#include <unordered_map>
 
 using namespace std;
 
 string progName;
 vector<pair<int, string>> objcode;
+vector<string> code;
 map<string, string> REG;
-map<string, pair<int, string>> OPTAB;
-map<string, int> SYMTAB;
+unordered_map<string, string> mnemonics;
+map<string, string> SYMTAB;
 int LOCCTR = 0, pc = 0, base = 0, index = 0;
+
+void create_mnemonics(){
+    mnemonics[(string)"ADD"] = (string)"18";
+    mnemonics[(string)"ADDR"] = (string)"58";
+    mnemonics[(string)"CLEAR"] = (string)"B4";
+    mnemonics[(string)"COMP"] = (string)"28";
+    mnemonics[(string)"COMPR"] = (string)"A0";
+    mnemonics[(string)"J"] = (string)"3C";
+    mnemonics[(string)"JEQ"] = (string)"30";
+    mnemonics[(string)"JLT"] = (string)"38";
+    mnemonics[(string)"JSUB"] = (string)"48";
+    mnemonics[(string)"LDA"] = (string)"00";
+    mnemonics[(string)"LDB"] = (string)"68";
+    mnemonics[(string)"LDCH"] = (string)"50";
+    mnemonics[(string)"LDT"] = (string)"74";
+    mnemonics[(string)"RD"] = (string)"D8";
+    mnemonics[(string)"RSUB"] = (string)"4C";
+    mnemonics[(string)"STA"] = (string)"0C";
+    mnemonics[(string)"STCH"] = (string)"54";
+    mnemonics[(string)"STL"] = (string)"14";
+    mnemonics[(string)"STX"] = (string)"10";
+    mnemonics[(string)"TD"] = (string)"E0";
+    mnemonics[(string)"TIXR"] = (string)"B8";
+    mnemonics[(string)"WD"] = (string)"DC";
+    mnemonics[(string)"A"] = (string)"0";
+    mnemonics[(string)"X"] = (string)"1";
+    mnemonics[(string)"S"] = (string)"2";
+    mnemonics[(string)"T"] = (string)"5";
+}
+
+/* Generating OPCODE symbol */
+void generate_optab() {
+
+}
+
+string get_label(string line) {
+    string label = "";
+    for(int i = 0; i < line.size(); i++) {
+        if(line[i] == ' ' || line[i] == '\t') {
+            break;
+        }
+        label += line[i];
+    }
+    return label;
+}
+
+string get_operator(string line) {
+    string opr = "";
+    for(int i = 11; i < line.size(); i++) {
+        if(line[i] == ' ' || line[i] == '\t') {
+            break;
+        }
+        opr += line[i];
+    }
+    return opr;
+
+}
+
+string get_operand(string line) {
+    string operand = "";
+    for(int i = 20; i < line.size(); i++) {
+        if(line[i] == ' ' || line[i] == '\t') {
+            break;
+        }
+        operand += line[i];
+    }
+    return operand;
+
+}
+
+string int_to_hex(int a) {
+    // convert integer to hex string
+    string hex = "";
+    while(a > 0) {
+        int rem = a % 16;
+        if(rem < 10) {
+            hex += (char)(rem + '0');
+        }
+        else {
+            hex += (char)(rem - 10 + 'A');
+        }
+        a /= 16;
+    }
+}
+
+int hex_to_int(string hex) {
+    // convert hex string to integer
+    int a = 0;
+    for(int i = 0; i < hex.size(); i++) {
+        if(hex[i] >= '0' && hex[i] <= '9') {
+            a = a * 16 + (hex[i] - '0');
+        }
+        else {
+            a = a * 16 + (hex[i] - 'A' + 10);
+        }
+    }
+    return a;
+}
+
+/* Generating Symbol Table */
+void generate_symtab() {
+    for(int i = 0; i < code.size(); i++){
+        string label = get_label(code[i]);
+        string opr = get_operator(code[i]);
+        string operand = get_operand(code[i]);
+
+        if (label != "" && opr == "EQU") {
+            if (operand == "*")
+                symtab.insert(make_pair(label, intermediate[i]));
+            else {
+                string a = "", b = "";
+                int flag = 0;
+                for (int i = 0; i < operand.size(); ++i) {
+                    if (operand[i] == '-') {
+                        flag = 1; continue;
+                    }
+                    if (flag == 0) a += operand[i];
+                    else b += operand[i];
+                }
+                symtab.insert(make_pair(label, int_to_hex(hex_to_int(symtab[a])-hex_to_int(symtab[b]))));
+            }
+        }
+        else if (label != "") {
+            symtab.insert(make_pair(label, intermediate[i]));
+        }
+    }
+}
+
+/* First pass of assembler */
+void pass1() {
+    // need to nenerate optab and symtab
+    // todo: generate_optab();
+    // todo: assign addresses to labels
+	generate_symtab();
+}
+
+/* Second pass of assembler */
+void pass2() {
+
+}
 
 int main(int argc, char *argv[]) {
     // Get cmd line args
@@ -26,6 +171,10 @@ int main(int argc, char *argv[]) {
     }
 
     inputFile.open(filePath.c_str());
-    
+
+    create_mnemonics();
+    pass1();
+    pass2();
+
 }
 
